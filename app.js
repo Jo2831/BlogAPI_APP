f348824dc842f0ab7ca39c1b9d18ad16b58dccc5
Yoseph Tapano <yosephman76@gmail.com>
2023-06-01 15:59:54 +0300

    app.js_file

---
+++ app.js
@@ -0,0 +1,14 @@
+import  express  from "express";
+import mongoose from "mongoose";
+import router from "./routes/user-route.js";
+import route from "./routes/blog-route.js";
+
+const app = express();
+
+app.use(express.json());
+app.use("/api/user", router);  
+app.use("/api/blog", route);
+mongoose.connect('mongodb+srv://admin:sayathappy@cluster0.bvenl7e.mongodb.net/?retryWrites=true&w=majority')
+.then(() => app.listen(5000))
+.then(() => console.log('Connected to database'))
+.catch((err) => console.log(err));

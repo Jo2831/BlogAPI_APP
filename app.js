9799d85e4c97999e2c37b1e65046c014a57caaeb
Yoseph Tapano <90330224+Jo2831@users.noreply.github.com>
2023-06-05 16:37:47 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use("/api/user", router);  
 app.use("/api/blog", route);
-mongoose.connect('mongodb+srv://admin:sayathappy@cluster0.bvenl7e.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://admin:<password>@cluster0.bvenl7e.mongodb.net/?retryWrites=true&w=majority')
 .then(() => app.listen(5000))
 .then(() => console.log('Connected to database'))
 .catch((err) => console.log(err));
